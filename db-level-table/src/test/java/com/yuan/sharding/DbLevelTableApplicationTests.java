4e1fc9ea5de858b5ebe212ada6dde0a45f1a937b
ayuanjm <2459087172@qq.com>
2020-07-19 01:31:23 +0800

    初始化项目

---
+++ db-level-table/src/test/java/com/yuan/sharding/DbLevelTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbLevelTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
